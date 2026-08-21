41289eef353ed2bf20032a2a210089fe78e3bf63
SRUSHTI DIPAK BHALEKAR <160705522+srushtibhalekar@users.noreply.github.com>
2026-08-21 23:59:41 +0530

    feat: simulate rolling two dice

--- DiceSimulator.java
+++ DiceSimulator.java
@@ -6,8 +6,13 @@ public class DiceSimulator {
 
         Random random = new Random();
 
-        int dice = random.nextInt(6) + 1;
+        int dice1 = random.nextInt(6) + 1;
+        int dice2 = random.nextInt(6) + 1;
 
-        System.out.println("You rolled: " + dice);
+        int total = dice1 + dice2;
+
+        System.out.println("🎲 Dice 1: " + dice1);
+        System.out.println("🎲 Dice 2: " + dice2);
+        System.out.println("Total: " + total);
     }
 }
